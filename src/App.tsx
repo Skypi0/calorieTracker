d71d4378897c2d093954bd3cbaa62934091fe0cb
“Xskypi0” <xskypi0@gmail.com>
2025-05-06 23:58:09 -0600

    feat: update index.html title and structure App component with header and section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import Form from "./components/Form"
 
 function App() {
 
   return (
     <>
-     <h1>Calculadora de Calorias</h1>
+     <header className="bg-lime-600 py-3">
+      <div className="max-w-4xl mx-auto flex justify-between items-center">
+        <h1 className="text-white uppercase font-bold text-center text-lg ">
+          Calorietracker
+        </h1>
+      </div>
+     </header>
+     <section className="bg-yellow-200 py-20 px-5">
+      <div className="max-w-4xl mx-auto">
+        <Form />
+      </div>
+     </section>
     </>
   )
 }
